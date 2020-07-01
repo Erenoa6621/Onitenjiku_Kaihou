88acbfabafb345097558d4414ef72dabe6b00656
ERENOA <m011823932@edu.teu.ac.jp>
2020-07-01 09:16:33 +0900

    a

---
+++ Kaihou_Onitenjiku/Assets/Scripts/SpeedMeter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedMeter : MonoBehaviour
+{
+    public float nowSpeed;
+    public Slider Meter;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Meter.maxValue = 20f;
+        Meter.minValue = 5f;
+
+        Meter.value = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowSpeed = player.GetComponent<Player>().nowSpeed;
+
+        Meter.value = nowSpeed;
+    }
+}
